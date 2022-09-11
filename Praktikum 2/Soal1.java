997e1ab2bd540240bd129eb5224e21a98157ce2a
Nitiya Rihadatul 'Aisy <104195287+Nitiya05@users.noreply.github.com>
2022-09-11 15:37:21 +0700

    Update Soal1.java

--- Praktikum 2/Soal1.java
+++ Praktikum 2/Soal1.java
@@ -5,7 +5,7 @@ public class Soal1 {
         double Num1 =3;
         double Num2 =2;
         
-        System.out.println("num1 + num2 = " + (num1 + num2) );
+        System.out.println("\nnum1 + num2 = " + (num1 + num2) );
         System.out.println("num1 - num2 = " + (num1 - num2) );
         System.out.println("num1 * num2 = " + (num1 * num2) );
         System.out.println("num1 / num2 = " + (Num1 / Num2) );
