7858939f1917aff0bffde0d99c3113ee3e400f55
Nitiya Rihadatul 'Aisy <104195287+Nitiya05@users.noreply.github.com>
2022-09-11 15:35:20 +0700

    Create Soal5.java

---
+++ Praktikum 2/Soal5.java
@@ -0,0 +1,9 @@
+public class Soal5{
+    public static void main(String[] args) {
+        for (int x=0;x<=20;x+=2){
+        if (x%6==0)
+        continue;
+        System.out.println(x);
+        }
+    }
+}
